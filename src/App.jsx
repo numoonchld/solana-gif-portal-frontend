756ec6fdc08370bf3069f3534e3959794a684ef1
Raghavendra Saralaya <raghavendra.saralaya@gmail.com>
2023-02-12 07:46:05 +0530

    moon portal ui shaping up

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import twitterLogo from "./assets/twitter-logo.svg";
 import "./App.css";
 
@@ -6,7 +6,18 @@ import "./App.css";
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
+const TEST_GIFS = [
+  "https://media.giphy.com/media/MolTU2s7zDKhi/giphy.gif",
+  "https://media.giphy.com/media/LMomqSiRZF3zi/giphy.gif",
+  "https://media.giphy.com/media/lk6ROvt7Hp2G4/giphy.gif",
+  "https://media.giphy.com/media/1wmbSPmlYzKINczi1A/giphy.gif",
+];
+
 const App = () => {
+  // State
+  const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+  const [gifList, setGifList] = useState([]);
   /*
    * This function holds the logic for deciding if a Phantom Wallet is
    * connected or not
@@ -15,10 +26,93 @@ const App = () => {
     // We're using optional chaining (question mark) to check if the object is null
     if (window?.solana?.isPhantom) {
       console.log("Phantom wallet found!");
+
+      /*
+       * The solana object gives us a function that will allow us to connect
+       * directly with the user's wallet
+       */
+      const response = await window.solana.connect({ onlyIfTrusted: true });
+      console.log("Connected with Public Key:", response.publicKey.toString());
+
+      /*
+       * Set the user's publicKey in state toView your GIF collection in the metaverse ✨ be used later!
+       */
+      setWalletAddress(response.publicKey.toString());
     } else {
       alert("Solana object not found! Get a Phantom Wallet 👻");
     }
   };
+
+  /*
+   * Let's define this method so our code doesn't break.
+   * We will write the logic for this next!
+   */
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log("Connected with Public Key:", response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const sendGif = async () => {
+    if (inputValue.length > 0) {
+      console.log("Gif link:", inputValue);
+      setGifList([...gifList, inputValue]);
+      setInputValue("");
+    } else {
+      console.log("Empty input. Try again.");
+    }
+  };
+
+  const onInputChange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+  };
+
+  /*
+   * We want to render this UI when the user hasn't connected
+   * their wallet to our app yet.
+   */
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      {/* Go ahead and add this input and button to start */}
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          sendGif();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter gif link!"
+          value={inputValue}
+          onChange={onInputChange}
+        />
+        <button type="submit" className="cta-button submit-gif-button">
+          Submit
+        </button>
+      </form>
+      <div className="gif-grid">
+        {gifList.map((gif) => (
+          <div className="gif-item" key={gif}>
+            <img src={gif} alt={gif} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 
   /*
    * When our component first mounts, let's check to see if we have a connected
@@ -32,14 +126,29 @@ const App = () => {
     return () => window.removeEventListener("load", onLoad);
   }, []);
 
+  useEffect(() => {
+    if (walletAddress) {
+      console.log("Fetching GIF list...");
+
+      // Call Solana program here.
+
+      // Set state
+      setGifList(TEST_GIFS);
+    }
+  }, [walletAddress]);
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
-          <p className="sub-text">
-            View your GIF collection in the metaverse ✨
-          </p>
+          <p className="header">🌕🌕🌕 Moon Portal 🌕🌕🌕</p>
+          <p className="sub-text">Everything related to the moon.</p>
+          <>
+            {/* Render your connect to wallet button right here */}
+            {!walletAddress && renderNotConnectedContainer()}
+            {/* We just need to add the inverse here! */}
+            {walletAddress && renderConnectedContainer()}
+          </>
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
